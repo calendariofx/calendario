276d4dfe771fc628a6ad0fd0af6ebb0d71a3f8e3
Devi Prasad <deviprsd21@gmail.com>
2015-03-27 01:42:28 +0530

    v4.0.0

--- js/data.js
+++ js/data.js
@@ -13,4 +13,4 @@ var events = {
 t = new Date(),
 //Creation of today event
 today = ((t.getMonth() + 1) < 10 ? '0' + (t.getMonth() + 1) : (t.getMonth() + 1)) + '-' + (t.getDate() < 10 ? '0' + t.getDate() : t.getDate()) + '-' +t.getFullYear();
-events[today] = [{content: 'TODAY', allDay: true}]
+events[today] = [{content: 'TODAY', allDay: true}];
