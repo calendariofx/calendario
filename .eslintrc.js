604a6911e808863b2c64c253fd4e0a3a9d168cf9
deviprsd <deviprsd21@gmail.com>
2019-08-01 10:50:20 -0500

    planning, and ready to begin development :hammer:

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+    'env': {
+        'es6': true,
+        'node': true,
+        'browser': true
+    },
+    'extends': 'eslint:recommended',
+    'globals': {
+        'Atomics': 'readonly',
+        'SharedArrayBuffer': 'readonly'
+    },
+    'parserOptions': {
+        'ecmaVersion': 2018,
+        'sourceType': 'module'
+    },
+    'rules': {
+        'indent': [
+            'error',
+            4
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'never'
+        ]
+    }
+}
